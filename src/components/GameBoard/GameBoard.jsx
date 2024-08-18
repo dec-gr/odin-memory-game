63ee81cb32117395b36f89150f194c62c03acfd4
Declan <144496393+dec-gr@users.noreply.github.com>
2024-08-18 14:29:15 +0100

    add font and text style

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -3,9 +3,15 @@ import styles from './GameBoard.module.css';
 export default function GameBoard(props) {
   return (
     <div className={styles.gameBoard}>
-      <div className="scoreBoard">
-        <h1>Score: {props.score}</h1>
-        <h1>High Score: {props.highScore}</h1>
+      <div className={styles.gameBoardHeader}>
+        <div className={styles.gameTitle}>
+          <h1>Pokemon Memory</h1>
+        </div>
+
+        <div className={styles.scoreBoard}>
+          <h2>Score: {props.score}</h2>
+          <h2>High Score: {props.highScore}</h2>
+        </div>
       </div>
       {props.children}
     </div>
